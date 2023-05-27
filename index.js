308fb9d1352d5d1dc1733630ff6914b53885acba
abideen-olawuwo <abideenolawuwo@gmail.com>
2023-05-27 11:50:23 +0100

    database

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL;
 
+mongoose.connect(mongoString);
+const database = mongoose.connection;
 
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
 const app = express();
+app.use(cors())
+app.use(express.json());
+
 
 
 app.listen(3000, () => {
